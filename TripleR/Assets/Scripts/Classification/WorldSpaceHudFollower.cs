f1de6e24da53cadefaba7930d2be31dfa079a0c1
Juan Ignacio Ojeda <juaniojeda2004@gmail.com>
2026-04-22 03:19:01 -0300

    UI

---
+++ TripleR/Assets/Scripts/Classification/WorldSpaceHudFollower.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public sealed class WorldSpaceHudFollower : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField, Min(0.2f)] private float distance = 1.25f;
+    [SerializeField] private float horizontalOffset = 0f;
+    [SerializeField] private float verticalOffset = 0.16f;
+    [SerializeField] private Vector3 hudScale = new Vector3(0.001f, 0.001f, 0.001f);
+    [SerializeField] private bool followPosition = true;
+    [SerializeField] private bool copyTargetRotation = true;
+    [SerializeField] private bool lookAtTargetHorizontally;
+
+    private Camera _cachedMainCamera;
+    private Canvas _canvas;
+
+    private void Awake()
+    {
+        _canvas = GetComponent<Canvas>();
+        ApplyUiLayerRecursively();
+        transform.localScale = hudScale;
+    }
+
+    private void LateUpdate()
+    {
+        Transform followTarget = ResolveTarget();
+        if (followTarget == null)
+            return;
+
+        if (followPosition)
+        {
+            Vector3 offset =
+                (followTarget.forward * distance) +
+                (followTarget.right * horizontalOffset) +
+                (followTarget.up * verticalOffset);
+
+            transform.position = followTarget.position + offset;
+        }
+
+        if (copyTargetRotation)
+            transform.rotation = followTarget.rotation;
+        else if (lookAtTargetHorizontally)
+            RotateTowardsTargetHorizontally(followTarget);
+
+        transform.localScale = hudScale;
+
+        if (_canvas != null && _canvas.worldCamera != _cachedMainCamera)
+            _canvas.worldCamera = _cachedMainCamera;
+    }
+
+    private Transform ResolveTarget()
+    {
+        if (target != null)
+            return target;
+
+        if (_cachedMainCamera == null || !_cachedMainCamera.isActiveAndEnabled)
+            _cachedMainCamera = Camera.main;
+
+        return _cachedMainCamera != null ? _cachedMainCamera.transform : null;
+    }
+
+    private void ApplyUiLayerRecursively()
+    {
+        int uiLayer = LayerMask.NameToLayer("UI");
+        if (uiLayer < 0)
+            return;
+
+        SetLayerRecursively(transform, uiLayer);
+    }
+
+    private static void SetLayerRecursively(Transform current, int layer)
+    {
+        current.gameObject.layer = layer;
+
+        for (int i = 0; i < current.childCount; i++)
+            SetLayerRecursively(current.GetChild(i), layer);
+    }
+
+    private void RotateTowardsTargetHorizontally(Transform followTarget)
+    {
+        Vector3 toHud = transform.position - followTarget.position;
+        toHud.y = 0f;
+
+        if (toHud.sqrMagnitude < 0.0001f)
+            return;
+
+        transform.rotation = Quaternion.LookRotation(toHud.normalized, Vector3.up);
+    }
+}
