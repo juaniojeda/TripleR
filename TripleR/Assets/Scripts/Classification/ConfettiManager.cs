7a0e18a81ae4f3dbad642dd96de9b940e89c16f1
Juan Ignacio Ojeda <juaniojeda2004@gmail.com>
2026-05-11 15:41:18 -0300

    0.0.2

---
+++ TripleR/Assets/Scripts/Classification/ConfettiManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+[RequireComponent(typeof(ClassificationContainer))]
+public class ConfettiManager : MonoBehaviour
+{
+    [Header("Configuración Visual")]
+    [SerializeField] private PooledParticle confettiPrefab;
+    [SerializeField] private int defaultPoolSize = 3;
+    [SerializeField] private int maxPoolSize = 10;
+
+    private ClassificationContainer container;
+    private IObjectPool<PooledParticle> particlePool;
+
+    private void Awake()
+    {
+        container = GetComponent<ClassificationContainer>();
+
+        // Inicializamos el Pool Nativo de Unity
+        particlePool = new ObjectPool<PooledParticle>(
+            createFunc: CreateParticle,
+            actionOnGet: (p) => p.gameObject.SetActive(true),
+            actionOnRelease: (p) => p.gameObject.SetActive(false),
+            actionOnDestroy: (p) => Destroy(p.gameObject),
+            collectionCheck: false,
+            defaultCapacity: defaultPoolSize,
+            maxSize: maxPoolSize
+        );
+    }
+
+    private void OnEnable()
+    {
+        // Nos suscribimos al evento del contenedor
+        container.OnCorrectClassification += SpawnConfetti;
+    }
+
+    private void OnDisable()
+    {
+        // Nos desuscribimos para evitar fugas de memoria
+        container.OnCorrectClassification -= SpawnConfetti;
+    }
+
+    private PooledParticle CreateParticle()
+    {
+        PooledParticle instance = Instantiate(confettiPrefab, transform);
+        instance.Initialize(particlePool);
+        return instance;
+    }
+
+    private void SpawnConfetti(Vector3 spawnPosition)
+    {
+        PooledParticle particle = particlePool.Get();
+        particle.PlayAt(spawnPosition);
+    }
+}
